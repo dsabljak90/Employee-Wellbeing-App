5247d9b3f2a53465c4d3e5a26fa093c11a3653ad
OliverTheHeavyCaliber <melicherciko@gmail.com>
2021-11-18 17:14:58 +0100

    foreach

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,18 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
+use App\Models\Response;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class TestController extends Controller
 {
     public function index()
     {
-        $questions = json_decode(file_get_contents(storage_path('questions.json')));
+        // $questions = json_decode(file_get_contents(storage_path('questions.json')));
+        $questions = Question::all();
         return view('test/test', ["questions"=>$questions]);
     }
-    public function store(Request $request)
-{
- redirect('/');
-}
+    public function store(Request $request) {
+        // do this as many times as you have questions
+        // common for all questions/responses in one submit 
+        foreach ($request->input('answers') as $question_id => $answer){
+        $response = new Response();
+        $response->testing_id = 2;
+        $response->user_id = Auth::user()->id;
+        
+        // specific for each question/answer
+        $response->question_id = $question_id;
+        $response->answer = $answer;
+         $response->save();
+        
 
+        }
+                dd($request->input('answers'));
+return $request->all();
+    return redirect('/');
+    }
+ 
+        
+
+ public function update(Request $request) {
+    foreach ($request->input('answers') as $question_id => $answer){
+        $response = new Response();
+        $response->test_id = 1;
+        $response->user_id = "??";
+        
+        // specific for each question/answer
+        $response->question_id = $question_id;
+        $response->answer = $answer;
+        // $response->save()
+        
+        dd($request->input('answers'));
+return $request->all();
+        }
+}
 }
--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -1,27 +1,29 @@
 
 <div>
     <p> Hello from Test Blade </p>
-    <form action="/test" method="POST"
+    <form action="/test" method="POST">
+    @csrf
     @foreach($questions as $key =>$question) 
         
-<p> {{$question}} </p>
+<p> {{$question->text}} </p>
     <label for="answer">1</label>
-<input type="radio" name="{{$key}}">
+<input type="radio" name="{{"answers[".$question->id."]"}}" value="1">
 
 <label for="answer">2</label>
-<input type="radio" name="{{$key}}">
+<input type="radio" name="{{"answers[".$question->id."]"}}" value="2">
 
     <label for="answer">3</label>
-<input type="radio" name="{{$key}}">
+<input type="radio" name="{{"answers[".$question->id."]"}}" value="3">
 
     <label for="answer">4</label>
-<input type="radio" name="{{$key}}">
+<input type="radio" name="{{"answers[".$question->id."]"}}" value="4">
 
     <label for="answer">5</label>
-    <input type="radio" name="{{$key}}">
+    <input type="radio" name="{{"answers[".$question->id."]"}}" value="5">
     <label for="answer">0</label>
-<input type="radio" name="{{$key}}">
-<input type="submit" value="Submit">
+<input type="radio" name="{{"answers[".$question->id."]"}}" value="0">
+
 @endforeach 
+<input type="submit" value="Submit">
 </form>
 </div>
